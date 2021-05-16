95ce179902c666e64e2f10c009d9a13762b6dec8
Soumojit Das <soumojitdas1999@gmail.com>
2021-05-16 12:34:52 +0530

    Test Completed

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
-export default {
-  api: {
-    baseUrl: 'https://api.spotify.com/v1',
-    authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
-  }
+const api = {
+  baseUrl: 'https://api.spotify.com/v1',
+  authUrl: 'https://accounts.spotify.com/api/token',
+  clientId: '1056b1a66330454ebe05716193321214',
+  clientSecret: '448bdf118a8c49648a4ca95b60d89e5e'
 }
+export { api };
--- src/routes/Discover/components/Discover.js
+++ src/routes/Discover/components/Discover.js
@@ -1,16 +1,58 @@
 import React, { Component } from 'react';
 import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
 import '../styles/_discover.scss';
+import axios from 'axios';
+import { api } from '../../../config';
 
 export default class Discover extends Component {
   constructor() {
     super();
 
     this.state = {
+      token: '',
       newReleases: [],
       playlists: [],
       categories: []
     };
+  }
+  componentDidMount = () => {
+    axios('https://accounts.spotify.com/api/token', {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + btoa(api.clientId + ':' + api.clientSecret)
+      },
+      data: 'grant_type=client_credentials',
+      method: 'POST'
+    })
+      .then(res => {
+        this.setState({ token: res.data.access_token });
+        console.log("Token", res.data.access_token)
+        axios('https://api.spotify.com/v1/browse/new-releases', {
+          method: 'GET',
+          headers: { 'Authorization': 'Bearer ' + this.state.token }
+        })
+          .then(res => {
+            this.setState({ newReleases: res.data.albums.items })
+          });
+      })
+      .then(res => {
+        axios('https://api.spotify.com/v1/browse/featured-playlists', {
+          method: 'GET',
+          headers: { 'Authorization': 'Bearer ' + this.state.token }
+        })
+          .then(res => {
+            this.setState({ playlists: res.data.playlists.items })
+          });
+      })
+      .then(res => {
+        axios('https://api.spotify.com/v1/browse/categories', {
+          method: 'GET',
+          headers: { 'Authorization': 'Bearer ' + this.state.token }
+        })
+          .then(res => {
+            this.setState({ categories: res.data.categories.items })
+          });
+      })
   }
 
   render() {
