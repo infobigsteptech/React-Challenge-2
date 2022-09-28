21001142818a7516b9d9b93d530904f59aaea000
madhav-CAF <madhav.tatikonda@sportgully.com>
2022-09-28 17:59:30 +0530

    [ok] done fetching three methods

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
-export default {
-  api: {
-    baseUrl: 'https://api.spotify.com/v1',
-    authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
+const Credentials = () => {
+
+  return {
+      ClientId: 'b64e1f99258e496eb29b9b2ef54b1fb8',
+      ClientSecret: 'eaa0615142dc44e4bbb4592fc4466f66'
   }
 }
+
+export { Credentials };
